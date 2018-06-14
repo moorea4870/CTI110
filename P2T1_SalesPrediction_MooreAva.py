c04e7c520f9ee5f7b897aa005d02f396206c3411
moorea4870 <40275548+moorea4870@users.noreply.github.com>
2018-06-14 14:03:15 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_MooreAva.py
@@ -0,0 +1,16 @@
+#CTI-110
+#P2T1 - Sales Prediction
+#Ava Moore
+#June 14, 2018
+
+#Sales Prediction Calculator
+
+#Declare some variables
+# variable     data type   Definition
+# totalSales     float     total sales
+# annualProfit   float     annual profit
+
+totalSales = float(input("Enter the projected amount of total sales: "))
+annualProfit = totalSales * 0.23
+
+print("Annual profit that will be made is $",annualProfit)
